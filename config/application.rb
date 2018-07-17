83727a7fe7729f5f8b206b38e4a8884020ce0436
awanijain <awanijain95@gmail.com>
2018-07-17 12:26:02 +0530

    precompile

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module MHDhaaba
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
